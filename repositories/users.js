6b3418bb88b2fa78beb5eb503c2c381a3503d298
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-04-08 11:23:38 -0700

    wrote getAll function

--- repositories/users.js
+++ repositories/users.js
@@ -6,14 +6,23 @@ class UsersRepository {
       throw new Error("Creating a repository requires a filename");
     }
     this.filename = filename;
-    try{
-    fs.accessSync(this.filename);
+    try {
+      fs.accessSync(this.filename);
     } catch (err) {
       fs.writeFileSync(this.filename, "[]");
-    }  
+    }
+  }
+  async getAll() {
+    const contents = await fs.promises.readFile(this.filename, {
+      encoding: "utf8",
+    });
+    console.log("contents", contents);
+  }
 }
-}
 
-const repo = new UsersRepository('users.json');
+const test = async () => {
+  const repo = new UsersRepository("users.json");
+  await repo.getAll();
+};
 
-
+test();
