5aeda3c8dcca55cf4a035e908c10f07210835891
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-04-10 11:25:56 -0700

    finished sign in, signup, sign out flow

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(
     keys: ["asdf1233kjhlkjhuyncbv"],
   })
 );
-app.get("/", (req, res) => {
+app.get("/signup", (req, res) => {
   res.send(`
     <div>
       <form method="POST">
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
         </div>`);
 });
 
-app.post("/", async (req, res) => {
+app.post("/signup", async (req, res) => {
   const { email, password, passwordConfirmation } = req.body;
   const existingUser = await usersRepo.getOneBy({ email });
   if (existingUser) {
@@ -35,6 +35,36 @@ app.post("/", async (req, res) => {
   req.session.userId = user.id;
 
   res.send("Account created");
+});
+
+app.get("/signout", (req, res) => {
+  req.session = null;
+  res.send("You are logged out");
+});
+
+app.get("/signin", (req, res) => {
+  res.send(`
+    <div>
+    <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirmation" placeholder="password confirmation" />
+        <button>Sign In</button>
+      </form>
+      </div>`);
+});
+
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await usersRepo.getOneBy({ email });
+  if (!user) {
+    return res.send("Email not found");
+  }
+  if (user.password !== password) {
+    return res.send("Invalid password");
+  }
+  req.session.userId = user.id;
+  res.send("You are signed in");
 });
 
 app.listen(3000, () => {
